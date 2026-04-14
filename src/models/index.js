8c5fae726ebbf841253d01afc5437af4b0759d50
Daniel Ixcot <daniel_ixcot@umes.edu.gt>
2026-04-13 20:51:20 -0600

    cambio a better-sqlite3 para render

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
+  dialectModule: require('better-sqlite3'),
   storage: './database.sqlite',
   logging: false
 });
