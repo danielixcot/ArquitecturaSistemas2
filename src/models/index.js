ec5ecce697e767e9168cf59ba10a5fc66a63b131
Daniel Ixcot <daniel_ixcot@umes.edu.gt>
2026-04-13 21:05:26 -0600

    cambio a postgresql para render

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  dialectModule: require('better-sqlite3'),
-  storage: './database.sqlite',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   logging: false
 });
 
